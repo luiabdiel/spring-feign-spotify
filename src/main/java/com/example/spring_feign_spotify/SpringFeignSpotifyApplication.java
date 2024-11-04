c252670aa59b580eeccdf224d7a72736cab12313
luiabdiel <luiggiabdieldev@gmail.com>
2024-11-03 21:14:58 -0300

    init

---
+++ src/main/java/com/example/spring_feign_spotify/SpringFeignSpotifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_feign_spotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFeignSpotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFeignSpotifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_feign_spotify/SpringFeignSpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_feign_spotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFeignSpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
